50629b718d008d1736d9efd5e22378eefd9520bc
Ricardo2500 <142996306+Ricardo2500@users.noreply.github.com>
2023-10-30 20:06:55 -0300

    Add files via upload

--- Quiz/js/script.js
+++ Quiz/js/script.js
@@ -261,9 +261,9 @@ function NextQuestion() {
 
 
 function renderResult() {
-    //esconde o card quiz
+    // Esconde o card quiz
     Card_Quiz.style.display = "none";
-    //exibe o card resultado
+    // Exibe o card resultado
     Card_Result.style.display = "block";
 
     //seleciona os elemntos html pelo seus ids
@@ -271,9 +271,15 @@ function renderResult() {
     const result_correct = document.getElementById("result-correct-answer");
     const total = document.getElementById("result-total-question");
 
-    //add texto dos elementos baseando nos resultados
+/*adicionado o texto dos elementos baseando nos resultados
+*/
+    // Exibe o total de questões
     total.innerText = Questions.length;
+
+    // Exibe o número de respostas corretas
     result_correct.innerText = Correct_Questions_value;
+
+    // Exibe a pontuação total do jogador
     ScoreResult.innerText = Points;
   }
 
@@ -297,7 +303,7 @@ function RestartQuiz(){
     // Representando a questão pelo numero + 1 para ser iniciado como 1 e não 0 do array
      Question_number.innerText = CurrentQuestion + 1;
    
-    //  Renderizando o quiz e trocando o display do card
+    // Renderizando o quiz e trocando o display do card
     renderQuestion();
     Card_Quiz.style.display = "block";
 }
